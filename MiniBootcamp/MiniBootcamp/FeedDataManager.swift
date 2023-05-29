9c2ec73f57b11f99d68f0a6f4e7ccdaea494a80c
Ikechukwu Onuorah <40242544+Lenever@users.noreply.github.com>
2023-05-29 16:32:56 +0100

    add more test

--- MiniBootcamp/MiniBootcamp/FeedDataManager.swift
+++ MiniBootcamp/MiniBootcamp/FeedDataManager.swift
@@ -11,16 +11,37 @@ protocol FeedDataManagerProtocol {
     func fetch(completion: @escaping (Result<[TweetCellViewModel], Error>) -> Void)
 }
 
+enum TweetAPIError: Error {
+    case noData
+    case response
+    case parsingData
+}
+
 class FeedDataManager: FeedDataManagerProtocol {
+  let session: URLSession
+
+  init(session: URLSession = URLSession.shared) {
+    self.session = session
+  }
+
     func fetch(completion: @escaping (Result<[TweetCellViewModel], Error>) -> Void) {
       let url = URL(string: "https://gist.githubusercontent.com/ferdelarosa-wz/0c73ab5311c845fb7dfac4b62ab6c652/raw/6a39cffe68d87f1613f222372c62bd4e89ad06fa/tweets.json")
       guard let url else { return }
-      let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+
+      let request = URLRequest(url: url)
+      let task = session.dataTask(with: request) { (data, response, error) in
+        if let error = error {
+            completion(.failure(error))
+            return
+        }
+
+
         let decoder = JSONDecoder()
         guard let data else {
-          completion(.failure(NSError(domain: "", code: 0)))
+          completion(.failure(TweetAPIError.noData))
           return
         }
+
         do {
           let jsonData = try decoder.decode(TweetsResponse.self, from: data)
           let tweets = jsonData.tweets.map { $0.mapToViewModel() }
@@ -28,7 +49,7 @@ class FeedDataManager: FeedDataManagerProtocol {
             completion(.success(tweets))
           }
         } catch {
-          completion(.failure(NSError(domain: "", code: 0)))
+          completion(.failure(TweetAPIError.parsingData))
         }
       }
       task.resume()
